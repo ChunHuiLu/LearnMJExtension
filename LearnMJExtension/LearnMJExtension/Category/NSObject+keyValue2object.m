85a82af2e2dd8c42611afee43bc3ba8df870940b
ChunHuiLu <33739105+ChunHuiLu@users.noreply.github.com>
2024-06-22 23:16:31 +0800

    JSON转模型

--- LearnMJExtension/LearnMJExtension/Category/NSObject+keyValue2object.m
+++ LearnMJExtension/LearnMJExtension/Category/NSObject+keyValue2object.m
@@ -17,20 +17,13 @@
 }
 
 - (instancetype)setKeyValues:(id)keyValues {
+    // 判断如果是JSON字符串还是NSData类型解析后转成模型
+    keyValues = [keyValues JSONObject];
     // 1.取出属性列表
     NSArray *propertiesArray = [self.class properties];
     for (MJProperty * property in propertiesArray) {
         MJPropertyType *type = property.type;
         Class typeClass = type.typeClass;
-//        if (type.isBoolType) {
-//            NSLog(@"Bool");
-//        } else if(type.isIdType) {
-//            NSLog(@"ID");
-//        } else if (type.isNumberType) {
-//            NSLog(@"Number");
-//        } else {
-//            NSLog(@"%@",typeClass);
-//        }
         // 2. 用该属性名作为键去字典中寻找对应的值
         id value = [keyValues valueForKey:property.name];
         if (!value) {continue;}
@@ -50,7 +43,7 @@
                 }
             }
         } else {
-            if (type.typeClass == [NSString class]) { // 属性类型是NSString
+            if (typeClass == [NSString class]) { // 属性类型是NSString
                 if ([value isKindOfClass:[NSNumber class]]) { // 值是NSNumber
                     // NSNumber -> NSString
                     value = [value description];
@@ -64,5 +57,15 @@
         [self setValue:value forKey:property.name];
     }
     return self;
+}
+
+- (id)JSONObject {
+    id foundationObj;
+    if ([self isKindOfClass:[NSString class]]) {
+        foundationObj = [NSJSONSerialization JSONObjectWithData:[(NSString *)self dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:nil];
+    } else if ([self isKindOfClass:[NSData class]]) {
+        foundationObj = [NSJSONSerialization JSONObjectWithData:(NSData *)self options:kNilOptions error:nil];
+    }
+    return foundationObj?:self;
 }
 @end
--- LearnMJExtension/LearnMJExtension/main.m
+++ LearnMJExtension/LearnMJExtension/main.m
@@ -13,11 +13,12 @@
 #pragma mark -- 函数声明
 void extcute(void (*fun)(void));
 void keyValue2object(void);
+void keyValues2object1(void);
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        extcute(keyValue2object);
+        extcute(keyValues2object1);
     }
     return 0;
 }
@@ -41,5 +42,18 @@ void keyValue2object(void) {
         @"gay":@"1"
     };
     User *user = [User objectWithKeyValues:dict];
-    
+    NSLog(@"name=%@, icon=%@, age=%d", user.name, user.icon, user.age);
+}
+/**
+ *  JSON字符串 -> 模型
+ */
+void keyValues2object1(void) {
+    // 1.定义一个JSON字符串
+    NSString *jsonString = @"{\"name\":\"Jack\", \"icon\":\"lufy.png\", \"age\":20}";
+
+    // 2.将JSON字符串转为User模型
+    User *user = [User objectWithKeyValues:jsonString];
+
+    // 3.打印User模型的属性
+    NSLog(@"name=%@, icon=%@, age=%d", user.name, user.icon, user.age);
 }
