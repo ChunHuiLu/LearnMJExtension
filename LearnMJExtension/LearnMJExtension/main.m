d1610312c5f0ae6f8b9d2dc2b69fe81d9c617b51
ChunHuiLu <33739105+ChunHuiLu@users.noreply.github.com>
2024-06-22 17:01:06 +0800

    初始化LearnMJExtension

---
+++ LearnMJExtension/LearnMJExtension/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearnMJExtension
+//
+//  Created by 卢春晖 on 2024/6/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
